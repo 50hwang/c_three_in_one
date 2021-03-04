c8504c9af9e5a242b0f639d01dda3cf9e118b15b
50hwang <gaenyum88@gmail.com>
2021-03-04 07:16:31 +0000

    successfully cloned into repl.it

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("\nHello, World!\n");
+	printf("\nI made one single .c file and successfully cloned into repl.it\n");
     return 0;
 }
