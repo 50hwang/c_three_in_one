fe96e308d259d78a10e44a3e43a2cd8075628581
50hwang <gaenyum88@gmail.com>
2021-03-04 07:21:04 +0000

    modified again at repl.it

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main() {
     printf("\nHello, World!\n");
 	printf("\nI made one single .c file and successfully cloned into repl.it\n");
     printf("\nThen, I reloaded with CLion to make sure if everything is right.\n");
+	printf("\nNext, I modified again at repl.it and ran.\n");
     return 0;
 }
