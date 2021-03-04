3e72b1e2f4027adf12bb8e3d3e4f4fa3de2e1975
50hwang <gaenyum88@gmail.com>
2021-03-04 07:26:21 +0000

    over AND OVER

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int main() {
 	printf("\nI made one single .c file and successfully cloned into repl.it\n");
     printf("\nThen, I reloaded with CLion to make sure if everything is right.\n");
 	printf("\nNext, I modified again at repl.it and ran.\n");
-    printf("\nFive, over again.\n");
+    printf("\nFive, over AND OVER again.\n");
     return 0;
 }
